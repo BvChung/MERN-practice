fd540798b9656b9630adbb52ea44c8a4d6c1b7db
Brandon Chung <chung.brandon123@gmail.com>
2022-03-01 20:57:59 -0600

    new file

---
+++ node.js
@@ -0,0 +1,9 @@
+// to run in node use (node node.js)
+
+// __dirname - path to current directory
+// __filename - file name
+// require  - function to use modules
+// module - infor about current module(file)
+// process - infor about env where the program is executed
+
+console.log("example");
